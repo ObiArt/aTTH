4a50d02dc1705011fc4984c347ab550ec5f3fe5e
ObiArt <58375833+ObiArt@users.noreply.github.com>
2021-08-21 18:15:08 +0300

    changed internal resolution

--- Params.cs
+++ Params.cs
@@ -6,8 +6,8 @@ namespace aTTH
     public static class Params
     {
         public static float _scale = 1.0f;
-        public const int _internalResolutionWidth = 200;
-        public const int _internalResolutionHeight = 120;
+        public const int _internalResolutionWidth = 150;
+        public const int _internalResolutionHeight = 90;
         public static bool _gamepadUsed = true;
         public static float _movementDeadzone = 0.25f;
         public static float _lookingDeadzone = 0.065f; //my gamepad is quite bad
